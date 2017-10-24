58feeb1c075f21224bdc6d11cc6c9e0cc064ba64
Lauren Cowen <lauren.cowen@gmail.com>
2017-10-23 21:13:15 -0500

    first challenge

---
+++ bamazon.js
@@ -0,0 +1,77 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'h78km9341ws?P',
+	database: 'bamazon'
+});
+
+connection.connect();
+connection.query('SELECT * FROM products', function(error, results){
+	if(error) throw error;
+	console.log("Item ID  Product Name  Price");
+	console.log("--------------------------------");
+	for(i =0; i<results.length; i++) {
+	console.log(results[i].item_id + "    " + results[i].product_name + "       $" + results[i].price.toFixed(2));
+	}
+	purchaseItem();
+})
+
+
+var purchaseItem = function() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'product',
+			message: 'Please enter the item ID you would like to purchase'
+
+		},
+
+		{
+			type: 'input',
+			name: 'qnty',
+			message: 'How many would you like to buy?'
+		}
+
+	]).then(function(answers){
+		checkQuantity(answers.product, answers.qnty);
+		
+	})
+};
+
+
+var checkQuantity = function(productID, quantity) {
+	connection.query('SELECT * FROM products WHERE item_id =?', [productID], function(error, results){
+		if(results[0].stock_quantity >= quantity) {
+			var totalPrice = results[0].price*quantity;
+			var newQuantity = results[0].stock_quantity - quantity;
+			console.log("new quantity: " + results[0].stock_quantity)
+			completePurchase(productID, newQuantity, totalPrice);
+		}
+
+		else {
+			console.log("Insufficient quantity!");
+			connection.end();
+		}
+	})
+};
+
+var completePurchase = function(productID, quantity, totalPrice) {
+	connection.query('UPDATE products SET ? WHERE ?', 
+		[
+		{
+			stock_quantity: quantity
+		}, 
+		{
+			item_id: productID
+		}
+	], function(error, result){
+		if(error) throw error;
+		console.log("Total Price: $" + totalPrice.toFixed(2));
+		 console.log(result.affectedRows + " products updated!\n");
+		connection.end();
+	})
+
+};
