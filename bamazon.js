53c45e37093a49abab1e8d02a56066a49dd79f13
Lauren Cowen <lauren.cowen@gmail.com>
2017-10-26 00:44:33 -0500

    fixed issue with database

--- bamazon.js
+++ bamazon.js
@@ -3,79 +3,74 @@ var mysql = require('mysql');
 var Table = require('cli-table');
 
 var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'h78km9341ws?P',
-	database: 'bamazon'
+    host: 'localhost',
+    user: 'root',
+    password: 'h78km9341ws?P',
+    database: 'bamazon'
 });
 
 connection.connect();
-connection.query('SELECT * FROM products', function(error, results){
-	if(error) throw error;
-	var table = new Table({
-    head: ['Item ID', 'Product Name', 'Price']
-  , colWidths: [10, 20, 10]
-});
+connection.query('SELECT * FROM products', function(error, results) {
+    if (error) throw error;
+    var table = new Table({
+        head: ['Item ID', 'Product Name', 'Price'],
+        colWidths: [10, 20, 10]
+    });
 
-	for(i =0; i<results.length; i++) {
-		table.push(
-    [results[i].item_id, results[i].product_name, '$' + results[i].price]
-);
-	}
-	console.log(table.toString());
-	purchaseItem();
+    for (i = 0; i < results.length; i++) {
+        table.push(
+            [results[i].item_id, results[i].product_name, '$' + results[i].price]
+        );
+    }
+    console.log(table.toString());
+    purchaseItem();
 })
 
 var purchaseItem = function() {
-	inquirer.prompt([
-		{
-			type: 'input',
-			name: 'product',
-			message: 'Please enter the item ID you would like to purchase'
+    inquirer.prompt([{
+            type: 'input',
+            name: 'product',
+            message: 'Please enter the item ID you would like to purchase'
 
-		},
+        },
 
-		{
-			type: 'input',
-			name: 'qnty',
-			message: 'How many would you like to buy?'
-		}
+        {
+            type: 'input',
+            name: 'qnty',
+            message: 'How many would you like to buy?'
+        }
 
-	]).then(function(answers){
-		checkQuantity(answers.product, answers.qnty);
-		
-	})
+    ]).then(function(answers) {
+        checkQuantity(answers.product, answers.qnty);
+
+    })
 };
 
 
 var checkQuantity = function(productID, quantity) {
-	connection.query('SELECT * FROM products WHERE item_id =?', [productID], function(error, results){
-		if(results[0].stock_quantity >= quantity) {
-			var totalPrice = results[0].price*quantity;
-			var newQuantity = results[0].stock_quantity - quantity;
-			completePurchase(productID, newQuantity, totalPrice);
-		}
-
-		else {
-			console.log("Insufficient quantity!");
-			connection.end();
-		}
-	})
+    connection.query('SELECT * FROM products WHERE item_id =?', [productID], function(error, results) {
+        if (results[0].stock_quantity >= quantity) {
+            var totalPrice = results[0].price * quantity;
+            var newQuantity = results[0].stock_quantity - quantity;
+            completePurchase(productID, newQuantity, totalPrice);
+        } else {
+            console.log("Insufficient quantity!");
+            connection.end();
+        }
+    })
 };
 
 var completePurchase = function(productID, quantity, totalPrice) {
-	connection.query('UPDATE products SET ? WHERE ?', 
-		[
-		{
-			stock_quantity: quantity
-		}, 
-		{
-			item_id: productID
-		}
-	], function(error, result){
-		if(error) throw error;
-		console.log("Total Price: $" + totalPrice.toFixed(2));
-		connection.end();
-	})
+    connection.query('UPDATE products SET ? WHERE ?', [{
+            stock_quantity: quantity
+        },
+        {
+            item_id: productID
+        }
+    ], function(error, result) {
+        if (error) throw error;
+        console.log("Total Price: $" + totalPrice.toFixed(2));
+        connection.end();
+    })
 
 };
--- bamazon.sql
+++ bamazon.sql
@@ -1,1 +1,22 @@
-SELECT * FROM bamazon.products;
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(40) NULL,
+price DECIMAL(10,2) NULL,
+stock_quantity INT(10) NULL,
+PRIMARY KEY (item_id)
+
+);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("hat", "clothing", 12.50, 50), ("pillow", "home", 10.00, 10),
+ ("milk", "food", 4.50, 25), ("bread", "food", 3.23, 20), ("socks", "clothing", 2.50, 12),
+ ("sheets", "home", 25.00, 5), ("tv", "electronics", 250.00, 2), ("cheese", "food", 1.50, 24),
+ ("peanuts", "food", 3.75, 15), ("T-shirt", "clothing", 15.00, 15);
